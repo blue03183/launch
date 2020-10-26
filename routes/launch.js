8c6845cb9712fa5c949b1885f7bc4eb2b979ad33
seo sang kwon <seosangkwon@Jeongui-MacBookPro.local>
2020-10-26 19:32:43 +0900

    상일상업고등학교 점심메뉴 조회

---
+++ routes/launch.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const moment = require('moment');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/menu', async (req, res, next) => {
+
+  const year = moment().format('YYYY');
+  const month =  moment().format('MM');
+  const day = moment().format('DD');
+
+  const siteData = await axios.get(`http://www.samil.hs.kr/main.php?menugrp=060501&master=meal2&act=list&SearchYear=${year}&SearchMonth=${month}&SearchDay=${day}`);
+  const pattern = /<th>중식 메뉴\s+<\/th>\s+<\/tr>\s+<\/thead>\s+<tbody>\s+<tr>\s+<td>\s+<p>\s+<img [^>]+>\s+<\/p>([^<]+)<\/td>/g;
+  const result = pattern.exec(siteData.data);
+  
+  res.send(result[1].trim());
+});
+
+module.exports = router;
