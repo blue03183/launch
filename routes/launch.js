0fe9d6168173ef78ab7f3aa8c064d74c4413a9f8
seo sang kwon <seosangkwon@Jeongui-MacBookPro.local>
2020-10-28 04:13:50 +0900

    123

--- routes/launch.js
+++ routes/launch.js
@@ -6,20 +6,20 @@ const router = express.Router();
 /* GET users listing. */
 router.get('/menu', async (req, res, next) => {
 
-  try {
-    const year = moment().format('YYYY');
-    const month =  moment().format('MM');
-    const day = moment().format('DD');
-  
-    const siteData = await axios.get(`http://www.samil.hs.kr/main.php?menugrp=060501&master=meal2&act=list&SearchYear=${year}&SearchMonth=${month}&SearchDay=${day}`);
-    const pattern = /<th>중식 메뉴\s+<\/th>\s+<\/tr>\s+<\/thead>\s+<tbody>\s+<tr>\s+<td>\s+<p>\s+<img [^>]+>\s+<\/p>([^<]+)<\/td>/g;
-    const result = pattern.exec(siteData.data);
+  // const year = moment().format('YYYY');
+  // const month =  moment().format('MM');
+  // const day = moment().format('DD');
+
+  // const siteData = await axios.get(`http://www.samil.hs.kr/main.php?menugrp=060501&master=meal2&act=list&SearchYear=${year}&SearchMonth=${month}&SearchDay=${day}`);
+  // const pattern = /<th>중식 메뉴\s+<\/th>\s+<\/tr>\s+<\/thead>\s+<tbody>\s+<tr>\s+<td>\s+<p>\s+<img [^>]+>\s+<\/p>([^<]+)<\/td>/g;
+  // const result = pattern.exec(siteData.data);
+  const result = await axios.get('https://www.plto.com');
+  console.log('----------------result----------------');
+  console.log(result.data);
+  res.send(result.data.trim()); 
     
-    res.send(result[1].trim()); 
-  } catch (error) {
-    console.log('----------------error----------------');
-    console.log(error);
-  }  
+  // res.send(result[1].trim()); 
+ 
 });
 
 module.exports = router;
