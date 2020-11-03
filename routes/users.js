6e99b2166711ebe3f4c6641687cac371b2f2c54c
seo sang kwon <seosangkwon@Jeongui-MacBookPro.local>
2020-11-03 20:07:18 +0900

    4

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,8 @@ router.post('/token', (req, res) => {
 router.post('/message', async (req, res) => {
   const message = '맥도날드 빅맥세트';
   const notifications = [];
-
+console.log('-----------------userTokens---------------');
+console.log(userTokens);
   for (const pushToken of userTokens) {
     console.log('----------------pushToken----------------');
     console.log(pushToken);
